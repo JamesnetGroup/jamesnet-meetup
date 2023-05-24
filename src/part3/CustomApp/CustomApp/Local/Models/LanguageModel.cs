4dee0924cdb602d244cfb8ac5ba2a1a4142d6e70
JAEWUNG LEE <james@jamesnet.dev>
2023-05-24 16:49:46 +0900

    Updates

---
+++ src/part3/CustomApp/CustomApp/Local/Models/LanguageModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomApp.Local.Models
+{
+    public class LanguageModel
+    {
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+
+        internal LanguageModel DataGen(string id, string name)
+        {
+            Id = id;
+            Name = name;
+            return this;
+        }
+    }
+}
---
+++ src/part3/CustomApp/CustomApp/Local/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CustomApp.Local.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomApp.Local.ViewModels
+{
+    public class MainViewModel
+    {
+        public List<LanguageModel> Languages { get; init; }
+
+        public MainViewModel()
+        {
+            Languages = GetLanguages();
+        }
+
+        private List<LanguageModel>? GetLanguages()
+        {
+            List<LanguageModel> source = new();
+            source.Add(new LanguageModel().DataGen("KOR", "Korean"));
+            source.Add(new LanguageModel().DataGen("ENG", "English"));
+            source.Add(new LanguageModel().DataGen("CHN", "Chinese"));
+            source.Add(new LanguageModel().DataGen("JPN", "Japanese"));
+            return source;
+        }
+    }
+}
