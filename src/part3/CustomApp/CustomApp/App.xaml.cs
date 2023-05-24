a9a2180367eaebb2374830f48f64d32e7461d285
JAEWUNG LEE <james@jamesnet.dev>
2023-05-24 17:34:39 +0900

    Updates

--- src/part3/CustomApp/CustomApp/App.xaml.cs
+++ src/part3/CustomApp/CustomApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CustomApp.Local.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,40 @@ namespace CustomApp
     /// </summary>
     public partial class App : Application
     {
+        private readonly Dictionary<string, ResourceDictionary> _languages;
+        private ResourceDictionary _currentLanguage;
+
+        public App()
+        {
+            _languages = new();
+            ResourceDictionary korean = GetResource("Korean");
+            ResourceDictionary english = GetResource("English");
+            ResourceDictionary chinese = GetResource("Chinese");
+            ResourceDictionary japanese = GetResource("Japanese");
+            _languages.Add("KOR", korean);
+            _languages.Add("ENG", english);
+            _languages.Add("CHN", chinese);
+            _languages.Add("JPN", japanese);
+
+            _currentLanguage = korean;
+            Resources.MergedDictionaries.Add(_currentLanguage);
+        }
+
+        internal void Switch(LanguageModel value)
+        {
+            ResourceDictionary current = _languages[value.Id];
+
+            Resources.MergedDictionaries.Remove(_currentLanguage);
+            Resources.MergedDictionaries.Add(current);
+            _currentLanguage = current;
+        }
+
+        private ResourceDictionary GetResource(string name)
+        {
+            ResourceDictionary resource = new();
+            resource.Source = new Uri($"/CustomApp;component/Themes/Cultures/{name}.xaml", UriKind.RelativeOrAbsolute);
+
+            return resource;
+        }
     }
 }
--- src/part3/CustomApp/CustomApp/Local/ViewModels/MainViewModel.cs
+++ src/part3/CustomApp/CustomApp/Local/ViewModels/MainViewModel.cs
@@ -1,15 +1,21 @@
-﻿using CustomApp.Local.Models;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CustomApp.Local.Models;
+using Jamesnet.Wpf.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CustomApp.Local.ViewModels
 {
-    public class MainViewModel
+    public partial class MainViewModel : ObservableBase
     {
         public List<LanguageModel> Languages { get; init; }
+
+        [ObservableProperty]
+        private LanguageModel _currentLanguage;
 
         public MainViewModel()
         {
@@ -24,6 +30,16 @@ namespace CustomApp.Local.ViewModels
             source.Add(new LanguageModel().DataGen("CHN", "Chinese"));
             source.Add(new LanguageModel().DataGen("JPN", "Japanese"));
             return source;
+        }
+
+        partial void OnCurrentLanguageChanged(LanguageModel value)
+        {
+            Application app = App.Current;
+
+            if (app is App myApp)
+            {
+                myApp.Switch(value);
+            }
         }
     }
 }
