7a974d7850e9a2178a4ca79a204ae4f3f658c506
JAEWUNG LEE <james@jamesnet.dev>
2023-06-22 16:35:55 +0900

    Updates

---
+++ src/part4/Infra/Infra/App.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Infra
+{
+    internal class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            Window window = new Window();
+            window.Title = "Infra Window!";
+            return window;
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
